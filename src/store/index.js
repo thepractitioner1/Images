571d6a03e0990f12940ecb384f77a00540dbac2a
thepractitioner1 <okerentiemisan@gmail.com>
2021-05-13 23:37:25 +0100

    Updated vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vuex from 'vuex';
 import Vue from 'vue';
 import auth from './modules/auth'
+import images from './modules/Images';
 
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules:{
-        auth
+        auth,
+        images
     }
 })
