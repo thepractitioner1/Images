0ecc52888af6380b1b7094420f25f94d91d9182b
thepractitioner1 <okerentiemisan@gmail.com>
2021-05-31 11:10:06 +0100

    Added lifecycle hook method

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -3,7 +3,15 @@
 </template>
 
 <script>
+import {mapActions} from 'vuex';
+
 export default {
   name: "ImageList",
+  methods:{
+    ...mapActions(['fetchImages'])
+  },
+  created(){
+    this.fetchImages();
+  }
 };
 </script>
